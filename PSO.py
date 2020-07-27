520a3d909b2d910256e09a09e24c83fc501b39a4
lishaogang <17631288710@163.com>
2020-07-27 16:51:16 +0800

    update mGAPSO

--- PSO.py
+++ PSO.py
@@ -29,7 +29,7 @@ class Particle:
         self.__vel = [random.uniform(-max_vel, max_vel) for i in range(dim)]  # 粒子的速度
         self.__bestPos = [0.0 for i in range(dim)]  # 粒子最好的位置
         self.__fitnessValue = fit_fun(self.__pos)  # 适应度函数值
-        self.__everBestValues = []  # 粒子曾经最好的适应值
+        self.__everBestValues = [fit_fun(self.__pos)]  # 粒子曾经最好的适应值
         self.__everBestPos = [] # 粒子曾经最好的位置
 
 
@@ -116,7 +116,10 @@ class PSO:
             for pi in everBestPos:
                 psum += pi[i]
                 cou += 1
-            pp = float(psum)/cou
+            if cou == 0:
+                pp = 0
+            else:
+                pp = float(psum)/cou
             vel_value = self.W * part.get_vel()[i] + self.C1 * random.random() * (pp - part.get_pos()[i]) \
                         + self.C2 * random.random() * (self.get_bestPosition()[i] + 2*self.x_max*boxmullersampling(mu=self.mu, sigma=self.sigma) - part.get_pos()[i])
             if vel_value > self.max_vel:
@@ -153,6 +156,7 @@ class PSO:
             for part in self.Particle_list:
                 self.update_vel(part)  # 更新速度
                 self.update_pos(part)  # 更新位置
+            print(i, ': ', self.get_bestFitnessValue())
             self.fitness_val_list.append(self.get_bestFitnessValue())  # 每次迭代完把当前的最优适应度存到列表
         return self.fitness_val_list, self.get_bestPosition()
 
--- mGAPSO.py
+++ mGAPSO.py
@@ -3,16 +3,26 @@
 # @author: lishaogang
 # @file: mGAPSO.py
 # @time: 2020/7/27 0027 15:31
-# @desc:
+# @desc:a micro-EA embeded in PSO
 
+import numpy as np
+import matplotlib.pyplot as plt
+from config import test_funcs
 from PSO import PSO
+import math
 
 dim = 30
-size = 30
-iter_num = 1500
-x_max = 100
-max_vel = 20
-
+size = 50
+iter_num = 2000
+max_vel = 10
+func_id = 10
+mu = 0
+sigma =10
 
 if __name__ == '__main__':
-    pso = PSO(dim, size, iter_num, x_max, max_vel, C1, C2, W, fit_func, mu =mu, sigma =sigma)
+    pso = PSO(dim, size, iter_num, test_funcs[func_id]['bound'], max_vel, W=1/(2*math.log10(2.0)),C1=0.5+math.log10(2.0),C2=0.5+math.log10(2.0), fit_func=test_funcs[func_id]['func'], mu =mu, sigma =sigma)
+    fit_var_list, best_pos = pso.update()
+    print("最优位置:" + str(best_pos))
+    print("最优解:" + str(fit_var_list[-1]))
+    plt.plot(np.linspace(0, iter_num, iter_num), fit_var_list)
+    plt.show()
