37e134aede80fc4c3cd45e173cfa5c79ff2afb3a
lishaogang <17631288710@163.com>
2020-07-27 15:30:45 +0800

    update PSO

--- PSO.py
+++ PSO.py
@@ -1,13 +1,26 @@
 #!/usr/bin/env python
 # encoding: utf-8
 # @author: lishaogang
-# @file: mGAPSO.py
+# @file: PSO.py
 # @time: 2020/7/24 0024 17:08
-# @desc:micro-GA embeded PSO
+# @desc:classical PSO algrithm
 
 import numpy as np
 import random
 
+
+def boxmullersampling(mu=0, sigma=1, size=1):
+    '''
+    产生一个高斯函数随机数
+    :param mu:
+    :param sigma:
+    :param size:
+    :return:
+    '''
+    u = np.random.uniform(size=size)
+    v = np.random.uniform(size=size)
+    z = np.sqrt(-2 * np.log(u)) * np.cos(2 * np.pi * v)
+    return mu + z * sigma
 
 class Particle:
     # 初始化
@@ -16,6 +29,9 @@ class Particle:
         self.__vel = [random.uniform(-max_vel, max_vel) for i in range(dim)]  # 粒子的速度
         self.__bestPos = [0.0 for i in range(dim)]  # 粒子最好的位置
         self.__fitnessValue = fit_fun(self.__pos)  # 适应度函数值
+        self.__everBestValues = []  # 粒子曾经最好的适应值
+        self.__everBestPos = [] # 粒子曾经最好的位置
+
 
     def set_pos(self, i, value):
         self.__pos[i] = value
@@ -38,12 +54,29 @@ class Particle:
     def set_fitness_value(self, value):
         self.__fitnessValue = value
 
+    def set_fitness_value2(self, value):
+        self.__fitnessValue = value
+        self.__everBestValues.append(value)
+
     def get_fitness_value(self):
         return self.__fitnessValue
 
+    def get_ever_best_values(self):
+        return self.__everBestValues
 
-class mGAPSO:
-    def __init__(self, dim, size, iter_num, x_max, max_vel, C1, C2, W, fit_func, best_fitness_value=float('Inf')):
+    def set_ever_best_values(self, item):
+        self.__everBestValues.append(item)
+
+    def get_ever_best_pos(self):
+        return self.__everBestPos
+
+    def set_ever_best_pos(self, pos):
+        self.__everBestPos.append(pos)
+
+
+
+class PSO:
+    def __init__(self, dim, size, iter_num, x_max, max_vel, C1, C2, W, fit_func, best_fitness_value=float('Inf'),mu=0.0, sigma=1.0):
         self.C1 = C1
         self.C2 = C2
         self.W = W
@@ -51,6 +84,8 @@ class mGAPSO:
         self.size = size  # 粒子个数
         self.iter_num = iter_num  # 迭代次数
         self.x_max = x_max
+        self.mu = mu
+        self.sigma = sigma
         self.max_vel = max_vel  # 粒子最大速度
         self.best_fitness_value = best_fitness_value
         self.best_position = [0.0 for i in range(dim)]  # 种群最优位置
@@ -74,9 +109,16 @@ class mGAPSO:
 
     # 更新速度
     def update_vel(self, part):
+        everBestPos = part.get_ever_best_pos()
         for i in range(self.dim):
-            vel_value = self.W * part.get_vel()[i] + self.C1 * random.random() * (part.get_best_pos()[i] - part.get_pos()[i]) \
-                        + self.C2 * random.random() * (self.get_bestPosition()[i] - part.get_pos()[i])
+            cou = 0
+            psum = 0
+            for pi in everBestPos:
+                psum += pi[i]
+                cou += 1
+            pp = float(psum)/cou
+            vel_value = self.W * part.get_vel()[i] + self.C1 * random.random() * (pp - part.get_pos()[i]) \
+                        + self.C2 * random.random() * (self.get_bestPosition()[i] + 2*self.x_max*boxmullersampling(mu=self.mu, sigma=self.sigma) - part.get_pos()[i])
             if vel_value > self.max_vel:
                 vel_value = self.max_vel
             elif vel_value < -self.max_vel:
@@ -97,9 +139,10 @@ class mGAPSO:
 
         value = self.fit_function(part.get_pos())
         if value < part.get_fitness_value():
-            part.set_fitness_value(value)
+            part.set_fitness_value2(value)
             for i in range(self.dim):
                 part.set_best_pos(i, part.get_pos()[i])
+            part.set_ever_best_pos(part.get_best_pos())
         if value < self.get_bestFitnessValue():
             self.set_bestFitnessValue(value)
             for i in range(self.dim):
@@ -113,7 +156,3 @@ class mGAPSO:
             self.fitness_val_list.append(self.get_bestFitnessValue())  # 每次迭代完把当前的最优适应度存到列表
         return self.fitness_val_list, self.get_bestPosition()
 
-
-
-if __name__ == '__main__':
-    pass
